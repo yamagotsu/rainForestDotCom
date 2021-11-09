7eefb0477c068ada40d0fd85b34e406b49ce50bf
Geraldo Moura <geraldo.moura@ymail.com>
2021-11-09 15:24:17 -0600

    added bootstrap and react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
@@ -5,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3>Hi, This is your Rainforest World!</h3>
+        <Button variant="secondary" className="mx-2">Button Example</Button>
       </header>
     </div>
   );
