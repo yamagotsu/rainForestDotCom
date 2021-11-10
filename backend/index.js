2854423d532bd67f07e89e418c122438fcfae9d3
Geraldo Moura <geraldo.moura@ymail.com>
2021-11-10 12:10:18 -0600

    db tables

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.get('/', (req, res) => res.send('Basic Server is up'));
 
-app.get('/', (req,res) => res.send('Basic Server is up'));
-
-
-app.listen(port, ()=> console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
