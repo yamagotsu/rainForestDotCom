165280f505de4f1586e1cd470b31c9375261a324
Geraldo Moura <geraldo.moura@ymail.com>
2021-11-16 11:12:35 -0600

    moved stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import FrontPage from './FrontPage';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <FrontPage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
